cd12388dd70e25ed032aa611f7627607332445d0
dperry233 <freemail40@yahoo.com>
2017-05-17 00:02:03 +0300

    syscalls (not finished)

--- hw2_syscalls.c
+++ hw2_syscalls.c
@@ -6,9 +6,9 @@ int sys_is_short(pid_t pid) {
 	if(!p){
 		return -3; // the value of ESRCH
 	}
-	if(p->sched_param == SCHED_SHORT){
+	if(p->policy == SCHED_SHORT&&p->overdue==0){
 		return 1; 
-	}else if(p->sched_param == SCHED_OVRD_SHORT){
+	}else if(p->policy == SCHED_SHORT&&p->overdue==1){
 		return 0;
 	}else{
 		return -22;   //the value of EINVAL
@@ -22,10 +22,10 @@ int sys_short_remaining_time(pid_t pid) {
 	if(!p){
 		return -3; // the value of ESRCH
 	}
-	if(p->policy == SCHED_SHORT){
+	if(p->policy == SCHED_SHORT&&p->overdue==0){
 		
 		return p->time_slice* HZ / 1000;  
-	}else if(p->policy == SCHED_OVRD_SHORT){
+	}else if(p->policy == SCHED_SHORT&&p->overdue==1){
 		
 		return p->time_slice* HZ / 1000;  
 	}else{
@@ -40,16 +40,16 @@ int sys_short_place_in_queue(pid_t pid) {
 		return -3; // the value of ESRCH
 	}
 	if(p->policy == SCHED_SHORT){
-	int i=0,j=0,sum=0;
-	struct list_head *pos;
+	int i=0,sum=0;
+	struct list_head * pos;
 		for(i=0;i<140;i++){
-			list_for_each(pos,p->array->queue+i){
+			list_for_each(pos,&((p->array)->queue[i])){
 				sum++;
-				if(list_entry(pos, struct task_struct, list))->pid==pid){
+				if(list_entry(pos, struct task_struct, run_list))->pid==pid){
 					return sum;
 				}
 			}
-
+			
 		}
 		return -10; // if it gets here then something is wrong
 		
