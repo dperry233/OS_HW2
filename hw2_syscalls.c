a628487c98b963893d08d468be331859debcc75e
dperry233 <freemail40@yahoo.com>
2017-05-16 18:23:49 +0300

    add syscall 3

--- hw2_syscalls.c
+++ hw2_syscalls.c
@@ -40,12 +40,22 @@ int sys_short_place_in_queue(pid_t pid) {
 		return -3; // the value of ESRCH
 	}
 	if(p->policy == SCHED_SHORT){
+	int i=0,j=0,sum=0;
+	struct list_head *pos;
+		for(i=0;i<140;i++){
+			list_for_each(pos,p->array->queue+i){
+				sum++;
+				if(list_entry(pos, struct task_struct, list))->pid==pid){
+					return sum;
+				}
+			}
+
+		}
+		return -10; // if it gets here then something is wrong
 		
+	
 		
-	}else if(p->policy == SCHED_OVRD_SHORT){
-		
-		
-	}else{
+	}else{ //if we are here it means p is not a short or overdue short
 		return -22;   //the value of EINVAL
 	}
 }
